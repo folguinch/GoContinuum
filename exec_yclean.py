50c840986f203a5dcb82e5f87610a0ae9fe5d2e7
Fernando Olguin <f.olguin.ch@gmail.com>
2019-07-04 12:59:46 +0800

    Modify YCLEAN source script

--- exec_yclean.py
+++ exec_yclean.py
@@ -4,6 +4,8 @@ from ConfigParser import ConfigParser
 import scipy
 import numpy as np
 
+from join_cubes import join_cubes
+
 def get_nchans(chanrange):
     i, f = map(int, ch.split('~'))
     return abs(f-i) + 1
@@ -26,6 +28,8 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', nargs=1, 
             help='Casa parameter.')
+    parser.add_argument('--basedir', default='./', type=str,
+            help='Base directory')
     parser.add_argument('uvdata', nargs=1, type=str,
             help='uv data ms')
     parser.add_argument('configfile', nargs=1, type=str,
@@ -55,42 +59,53 @@ def main():
             for j in range(len(chanranges))])
 
     # Clean options
-    gridder='standard'
+    gridder = 'standard'
     wprojplanes = None 
     specmode = 'cube'
-    outframe='LSRK'
+    outframe = 'LSRK'
     interpolation = 'linear'
     interactive = False
     imsize = map(int, config.get(section, 'imsize').split())
     cell = str(config.get(section, 'cellsize'))
     weighting = 'briggs'
     robust = 0.5
-    deconvolver='multiscale' 
-    scales=[0,5,15]
+    deconvolver = 'multiscale' 
+    scales = [0,5,15]
     #rms = 5.7e-3
     #peak_int = 0.136# these variables are not used apparently
     #SN_ratio =  26 # these variables are not used apparently
-    chanchunks=2
-    limitMaskLevel=4.0
-    pblimit=.2
+    chanchunks = 2
+    limitMaskLevel = 4.0
+    pblimit = .2
 
-    if os.path.isdir()
-
+    # Run YCLEAN
+    finalcubes = []
     for moldata in molecules:
-        it=0
-        mol=moldata[0]
-        restfreq=moldata[1]
-        dirmol='yclean/'+source+'_'+mol+'/'
-        os.system('rm -rf '+dirmol)
-        width=moldata[2]
-        start=moldata[3]
-        molvis=moldata[5]
-        nchan=int(moldata[4])
-        spwline=moldata[6]
+        it = 0
+        mol = moldata[0]
+        restfreq = moldata[1]
+        dirmol = os.path.join(args.basedir, 'yclean', source+'_'+mol)
+        os.system('rm -rf '+ dirmol)
+        width = moldata[2]
+        start = moldata[3]
+        molvis = moldata[5]
+        nchan = int(moldata[4])
+        spwline = moldata[6]
         print "Procesing ", mol
-        vis = args.uvdata
-        imagename = dirmol+'auto'+source+'_'+mol+'.12m'
+        vis = args.uvdata[0]
+        imagename = os.path.join(dirmol, 'auto'+source+'_'+mol+'.12m')
         print vis, imagename
 
         execfile(os.path.join(diryclean, 'yclean_parallel.py'))
+        finalcubes += [imagename+'.tc_final.fits']
+
+    # Join the cubes
+    nsub = len(chanranges)
+    for i in range(len(freqs)):
+        output = os.path.join(args.basedir, 'clean',
+                source+'.spw%i.cube' % i)
+        j = i*nsub
+        join_cubes(finalcubes[j:j+nsub], output, 
+                config.get('yclean','joinchans').split)
+
         
