19c5e1e70a1f15c4b69909382042efe6d918aacc
Fernando Olguin <f.olguin.ch@gmail.com>
2019-07-15 16:35:53 +0800

    Improve logging and fix errors

--- pipeline.sh
+++ pipeline.sh
@@ -6,31 +6,50 @@
 #
 #
 
-set -eu
+set -e
 
 sep1="================================================================================"
 sep2="--------------------------------------------------------------------------------"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+VERBOSE=1
 
 # Functions
 function usage () {
     echo "Nothing"
 }
 
+function logger (){
+    local level="INFO"
+    if [[ $1 != "" ]]
+    then
+        if [[ $1 == "INFO" ]] || [[ $1 == "WARN" ]] || [[ $1 == "ERROR" ]]
+        then
+            level="$1"
+            shift
+        fi
+    fi
+
+    if [[ $VERBOSE -eq 1 ]]
+    then
+        echo "${level}: $@" | tee -a "${LOGGERFILE}"
+    fi
+}
+
+
 function check_environment () {
     if [[ ! -d $Dirty ]]
     then
-        echo "Could not find dirty directory"
+        logger "ERROR" "Could not find dirty directory"
         exit 1
     fi
     if [[ ! -d $Plots ]]
     then
-        echo "Creating plots directory"
+        logger "Creating plots directory"
         mkdir $Plots
     fi
     if [[ ! -d $PBclean ]]
     then
-        echo "Creating pbclean directory"
+        logger "Creating pbclean directory"
         mkdir $PBclean
     fi
 }
@@ -44,11 +63,11 @@ function get_spectra () {
     local script="$DIR/extract_spectra.py"
     if [[ $Method == "position" ]]
     then
-        echo "Extracting spectrum at: ${Xpos}, ${Ypos}"
+        logger "Extracting spectrum at: ${Xpos}, ${Ypos}"
         local flags="$Method $1 $2 $Xpos $Ypos"
     elif [[ $Method == "max" ]]
     then
-        echo "Extracting spectrum at maximum"
+        logger "Extracting spectrum at maximum"
         local flags="--niter 1 --image_file ${1/.fits/.max.fits}"
         flags="$flags $Method --pos_file $3 $2 $1"
     fi
@@ -61,15 +80,16 @@ function combine_peaks () {
 }
 
 function get_continuum_channels () {
+    local script="$DIR/continuum_iterative.py"
     for specfile in $*
     do
-        local script="$DIR/continuum_iterative.py"
         local plotname="${specfile/dirty/plots}"
         local flags="--table ${BASE}pipeline_${method}_continuum.dat"
         flags="$flags --tableinfo $(basename $specfile)"
         flags="$flags --plotname ${plotname/.dat/.png}"
         flags="$flags --chanfile ${specfile/.dat/.chanfile.txt}"
-        flags="$flags --min_width 2"
+        flags="$flags --min_width 2" 
+        flags="$flags --config ${BASE}${SRC0}.cfg" 
         flags="$flags --spec ${specfile} sigmaclip --sigma 3.0 1.3"
         echo $sep2
         python $script $flags 
@@ -82,7 +102,7 @@ function get_peak_continuum_channels () {
     then
         # First pass
         local counter=0
-        echo "Extracting spectra"
+        logger "Extracting spectra (1st pass)"
         for dirt in ${Dirty}/${SRC}*.image.fits
         do
             local specbase=${dirt/.fits/}
@@ -97,26 +117,28 @@ function get_peak_continuum_channels () {
                     rm -f $posfile
                 elif [[ $Redo -eq 0 ]] && [[ -f $posfile ]]
                 then
-                    echo "File $(basename $posfile) already created"
+                    logger "File $(basename $posfile) already created"
                     break
                 fi
                 counter=1
             fi
             # Find position of maxima
             echo $sep2
-            echo "Working on: $(basename $dirt)"
+            logger "Working on: $(basename $dirt)"
             get_spectra $dirt $specbase $posfile
         done
+        logger "Extracting spectra done"
 
         # Combine peaks
         echo $sep2
-        echo "Combining peaks"
+        logger "Combining peaks"
         local combposfile="${posfile/.dat/.combined.dat}"
         if [[ $Redo -eq 1 ]] || [[ ! -f $combposfile ]]
         then
             combine_peaks $posfile $combposfile
+            logger "Done combining peaks"
         else
-            echo "File $(basename $combposfile) already created"
+            logger "File $(basename $combposfile) already created"
         fi
 
         # Assign source position values
@@ -135,18 +157,20 @@ function get_peak_continuum_channels () {
         done
     fi
     # Second pass
+    logger "Extracting spectra (2nd pass)"
     for dirt in ${Dirty}/${SRC}*.image.fits
     do
         echo $sep2
-        echo "Extracting spectra"
-        echo "Working on: $(basename $dirt)"
+        logger "Extracting spectra"
+        logger "Working on: $(basename $dirt)"
         specbase=${dirt/.fits/}
         get_spectra $dirt $specbase
         
-        echo "Getting continuum channels"
+        logger "Getting continuum channels"
         local specfiles="${specbase}.p0spec.dat"
         get_continuum_channels $specfiles
     done
+    logger "Done with AFOLI"
 
     # Restore method
     if [[ "$methodin" != "$Method" ]]
@@ -157,22 +181,27 @@ function get_peak_continuum_channels () {
 
 function run_uvcontsub () {
     local script="$DIR/run_uvcontsub.py"
+    local casalogfile="$LOGS/casa_$(date --utc +%F_%H%M%S)_run_uvcontsub.log"
     if [[ $Redo -eq 1 ]] || [[ ! -d "$1.contsub" ]]
     then
         if [[ -d "$1.contsub" ]]
         then
-            echo "Removing ${1}.contsub"
+            logger "Removing ${1}.contsub"
             rm -rf "${1}.contsub"
         fi
-        casa -c $script $*
+        logger "Running uvcontsub"
+        casa --logfile $casalogfile -c $script $*
+        logger "uvcontsub succeded"
     elif [[ -d "$1.contsub" ]]
     then
-        echo "Directory ${1}.contsub already exists"
+        logger "Directory ${1}.contsub already exists"
     fi
 }
 
 function run_pbclean () {
     local script="$DIR/pbclean.py"
+    local casalogfile="$LOGS/casa_$(date --utc +%F_%H%M%S)_pbclean.log"
+    local casaflags="--logfile $casalogfile -c"
     local config="${BASE}${SRC0}.cfg"
     if [[ $2 -eq 0 ]]
     then
@@ -191,12 +220,14 @@ function run_pbclean () {
             then
                 if [[ -d "${imgname}.image" ]]
                 then
-                    echo "Removing pb cleaned image: $(basename $imgname)"
+                    logger "Removing pb cleaned image: $(basename $imgname)"
                     rm -rf "${imgname}.*"
                 fi
-                mpicasa -n 5 $(which casa) -c $script --nothreshold --spw $spw $1 $imgname $config
+                logger "Runnung pbclean"
+                mpicasa -n 5 $(which casa) $casaflags $script --nothreshold --spw $spw $1 $imgname $config
+                logger "Pbclean succeded"
             else
-                echo "Image ${imgname}.image already exists"
+                logger "Image ${imgname}.image already exists"
             fi
             spw=$((spw + 1))
             echo $sep2
@@ -208,12 +239,14 @@ function run_pbclean () {
         then
             if [[ -d "${imgname}.image" ]]
             then
-                echo "Removing pb cleaned image: $(basename $imgname)"
+                logger "Removing pb cleaned image: $(basename $imgname)"
                 rm -rf "${imgname}.*"
             fi
-            mpicasa -n 5 $(which casa) -c $script --nothreshold --continuum $1 $imgname $config
+            logger "Runnung pbclean"
+            mpicasa -n 5 $(which casa) $casaflags $script --nothreshold --continuum $1 $imgname $config
+            logger "Pbclean succeded"
         else
-            echo "Image ${imgname}.image already exists"
+            logger "Image ${imgname}.image already exists"
         fi
     fi
 
@@ -222,11 +255,14 @@ function run_pbclean () {
 function run_yclean () {
     local script="$DIR/exec_yclean.py"
     local config="${BASE}${SRC0}.cfg"
+    local casalogfile="$LOGS/casa_$(date --utc +%F_%H%M%S)_exec_yclean.log"
+    
+    cd $BASE
     if [[ $Redo -eq 1 ]] || [[ ! -d $YCLEAN ]]
     then
         if [[ -d $YCLEAN ]]
         then
-            echo "Emptying YCLEAN directory: $YCLEAN"
+            logger "Emptying YCLEAN directory: $YCLEAN"
             rm -rf ${YCLEAN}/*
             rm -rf *MASCARA.tc*
         else
@@ -234,37 +270,46 @@ function run_yclean () {
         fi
         if [[ -d $CLEAN ]]
         then
-            echo "Emptying CLEAN directory: $CLEAN"
+            logger "Emptying CLEAN directory: $CLEAN"
             rm -rf ${CLEAN}/*.cube*
         else
             mkdir $CLEAN
         fi
-        mpicasa -n 3 $(which casa) -c $script --basedir $BASE $1 $config
+        logger "Running YCLEAN"
+        mpicasa -n 3 $(which casa) --logfile $casalogfile -c $script $1 $config
+        logger "YCLEAN succeded"
     else
-        echo "YCLEAN already ran"
+        logger "YCLEAN already ran"
     fi
+    cd -
 }
 
 function split_continuum () {
     local script="$DIR/split_ms.py"
+    local casalogfile="$LOGS/casa_$(date --utc +%F_%H%M%S)_split_ms.log"
     local splitfile1="${1}.cont_avg"
     local splitfile2="${1}.allchannels_avg"
     if [[ $Redo -eq 1 ]] || [[ ! -d $splitfile1 ]]
     then
         if [[ -d $splitfile1 ]]
         then
-            echo "Removing continuum split files"
+            logger "Removing continuum split files"
             rm -rf $splitfile1 $splitfile2
         fi
-        casa -c $script $*
+        logger "Splitting the ms"
+        casa --logfile $casalogfile -c $script $*
+        logger "Splitting succeded"
     else
-        echo "Files $splitfile1 and $splitfile2 already exist"
+        logger "Files $splitfile1 and $splitfile2 already exist"
     fi
 }
 
 function concat_vis () {
     local script="$DIR/run_concat.py"
-    casa -c $script $*
+    local casalogfile="$LOGS/casa_$(date --utc +%F_%H%M%S)_run_concat.log"
+    logger "Concatenating: $*"
+    casa --logfile $casalogfile -c $script $*
+    logger "Concatenation succeded"
 }
 
 function line_pipe () {
@@ -279,12 +324,12 @@ function line_pipe () {
         then
             if [[ -d $concatms ]]
             then
-                echo "Removing concatenated line file: $(basename $concatms)"
+                logger "Removing concatenated line file: $(basename $concatms)"
                 rm -rm $concatms
             fi
             concat_vis $concatms $@
         else
-            echo "File $(basename $concatms) already created"
+            logger "File $(basename $concatms) already created"
         fi
         echo $sep2
     fi
@@ -305,12 +350,12 @@ function continuum_pipe () {
         then
             if [[ -d $concatms ]]
             then
-                echo "Removing concatenated continuum file"
+                logger "Removing concatenated continuum file"
                 rm $concatms
             fi
             concat_vis $concatms $@
         else
-            echo "File $(basename $concatms) already created"
+            logger "File $(basename $concatms) already created"
         fi
         echo $sep2
     fi
@@ -343,7 +388,7 @@ function main () {
         else
             SRC="${SRC0}"
         fi
-        echo "Working on source: $SRC"
+        logger "Working on source: $SRC"
         get_peak_continuum_channels 
         echo $sep2
         
@@ -373,7 +418,7 @@ function main () {
 }
 
 # Command line options
-BASE=""
+BASE="./"
 PutRms=0
 Redo=1
 Method="max"
@@ -423,6 +468,12 @@ UVdata="${BASE}final_uvdata"
 PBclean="${BASE}pbclean"
 CLEAN="${BASE}clean"
 YCLEAN="${BASE}yclean"
+LOGS="${BASE}logs"
+if [[ ! -d $LOGS ]]
+then
+    mkdir $LOGS
+fi
+LOGGERFILE="$LOGS/pipeline_debug.log"
 
 main
 echo "QMD"
